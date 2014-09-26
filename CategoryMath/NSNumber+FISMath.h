2b9edc584fb095fa50bb8e6c7fc498eed01fa536
Chris Gonzales <gonzales.chris.a@gmail.com>
2014-09-26 11:37:10 -0400

    add NSNumber+FISMath category file

---
+++ CategoryMath/NSNumber+FISMath.h
@@ -0,0 +1,13 @@
+//
+//  NSNumber+FISMath.h
+//  CategoryMath
+//
+//  Created by Chris Gonzales on 9/26/14.
+//  Copyright (c) 2014 The Flatiron School. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSNumber (FISMath)
+
+@end
---
+++ CategoryMath/NSNumber+FISMath.m
@@ -0,0 +1,13 @@
+//
+//  NSNumber+FISMath.m
+//  CategoryMath
+//
+//  Created by Chris Gonzales on 9/26/14.
+//  Copyright (c) 2014 The Flatiron School. All rights reserved.
+//
+
+#import "NSNumber+FISMath.h"
+
+@implementation NSNumber (FISMath)
+
+@end
