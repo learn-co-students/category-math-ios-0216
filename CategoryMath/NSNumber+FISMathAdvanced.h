c72c6cf371b4d639d603abd293e9df8058fcf88c
Chris Gonzales <gonzales.chris.a@gmail.com>
2014-11-14 14:54:25 -0500

    add advanced solution

---
+++ CategoryMath/NSNumber+FISMathAdvanced.h
@@ -0,0 +1,18 @@
+//
+//  NSNumber+FISMathAdvanced.h
+//  CategoryMath
+//
+//  Created by Chris Gonzales on 11/12/14.
+//  Copyright (c) 2014 The Flatiron School. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSNumber (FISMathAdvanced)
+
+-(NSNumber *)advancedAdd:(NSNumber *)number;
+-(NSNumber *)advancedSubtract:(NSNumber *)number;
+-(NSNumber *)advancedMultiplyBy:(NSNumber *)number;
+-(NSNumber *)advancedDivideBy:(NSNumber *)number;
+
+@end
